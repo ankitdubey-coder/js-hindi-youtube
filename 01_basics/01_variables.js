7e09626eedb12922ffa8acfdd492aa566fb2a19a
ankitdubey-coder <145497149+ankitdubey-coder@users.noreply.github.com>
2024-08-15 08:23:45 +0000

    datatype

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,25 @@
+const accountId = 144553
+let accountEmail =" ankitdubey7525@gmail.com"
+var accountPassword = "12345"
+accountCity = "Pratapgarh"
+let accountstate;
+
+// accountId = 2 // not allowed
+
+console.log(accountId);
+
+
+
+accountEmail = "ankittechtide0007@gmail.com"
+accountPassword = "28762"
+accountCity = "Noida"
+
+
+/*
+Prefer not to use vae
+becauce of issue in block scope and functional scope
+*/
+
+
+console.table ([accountEmail,accountPassword,accountCity,accountstate])
+
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,34 @@
+"use strict"; // Treat all js code as newer version
+
+// alert(3+3) // we are using node.js not browser
+
+console.log(3
+     + 3); // code readability should be high
+
+     console.log("Ankit");
+
+     let name = "Ankit"
+     let age = 20
+     let isLoggedIn = false
+
+
+     // Number => 2 to power 53
+     // Bigint
+     // string => ""
+     // boolean  true/false
+     // Null => standalone value 
+     // undefined => abhi value asign nhi hui hai 
+     // Symbol  => unique
+
+
+     // Object
+
+     console.log(typeof undefined);  // undefined
+     console.log(typeof null);       // object
+     
+
+
+     
+     
+
+
