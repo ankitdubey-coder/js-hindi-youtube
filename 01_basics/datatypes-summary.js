93bdba82d98994d9e58032775dd797b6e2225a79
Ankit Dubey <ankitdubey7525@gmail.com>
2024-08-16 11:42:34 +0530

    datatypesummary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,43 @@
+// Primitive
+// 7 types : String, Number, Boolean, Null, undifined, symbol, BigInt,
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+
+let userEmail;
+const id = Symbol('1234')
+const anotherId = Symbol('1234')
+// console.log( id === anotherId);
+
+//const bigNumber = 657438922345876542345698765n
+
+
+
+
+
+
+
+
+
+
+// Referencs (Non primitive)
+
+// Array, Objects, Function
+
+const heros = ["saktiman","nagraj","doga"];
+
+ let myObj =  {
+    name:"ankit",
+    age:  20,
+}
+
+ const myFunction =  function(){
+    console.log("hello World");
+    
+ }
+
+ console.log(typeof bigNumber);
+ 
